756a52cc2852976dea090c6756138d6ad3dd1dcf
carlos <cadumocinho67@gmail.com>
2023-11-03 23:45:47 -0300

    eduarda

--- boneco.py
+++ boneco.py
@@ -1,19 +1,29 @@
+# Definição da classe Cabeca
 class Cabeca:
     def _init_(self):
+        # Inicializa o atributo estado como "Intacta"
         self.estado = "Intacta"
 
     def destruir(self):
+        # Muda o estado para "Destruída" quando o método destruir é chamado
         self.estado = "Destruída"
+        # Imprime uma mensagem informando que a cabeça foi destruída
         print("A cabeça foi destruída!")
 
+# Definição da classe Boneco
 class Boneco:
     def _init_(self):
+        # Inicializando o atributo cabeca como uma instância da classe Cabeca
         self.cabeca = Cabeca()
 
     def destruir(self):
+        # Chama o método destruir da instância de Cabeca associada ao boneco
         self.cabeca.destruir()
-        print("O boneco foi destruído!")
+        # Imprime uma mensagem informando que o boneco foi destruído
 
-if __name__ == "_main_":
+# Bloco de código a ser executado apenas se este arquivo for executado diretamente
+if _name_ == "_main_":
+    # Cria um objeto da classe Boneco
     boneco = Boneco()
+    # Chama o método destruir do boneco, que também chama o método destruir da cabeça
     boneco.destruir()
